36be72888807e5f79ad2d3b9d38fe65b07e50a89
Muhammad Haikal <muhammad.haikal494972@gmail.com>
2025-01-05 02:18:59 +0700

    Tilemap, Lerp di kamera

---
+++ Scenes/Camera/player_camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var player_node 
+var lokasi_player
+
+func _ready() -> void:
+	player_node = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta: float) -> void:
+	kamera_ke_player()
+	
+func kamera_ke_player():
+	if player_node :
+		lokasi_player = player_node.global_position
+		#global_position = global_position.lerp(lokasi_player, 1.0 - pow(-delta ,10))
+		global_position = lerp(global_position, lokasi_player,get_physics_process_delta_time() * 7)
